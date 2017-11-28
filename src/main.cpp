695178828374add56537a003d4fe5a8e07b7b09f
Dustin Ryan - Roepsch <dustinroepsch@gmail.com>
2017-11-28 15:51:18 -0600

    Registers appear to be working

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,25 @@
-#include <iostream>
+#include <cstdio>
+
+#include "Registers/Registers.hpp"
 
 int main() {
-    std::cout << "Gemuboi Hello!" << std::endl;
+    Registers reg;
+
+    printf("0x%02x\n", reg.getA());
+    reg.setA(0x1B);
+    printf("0x%02x\n", reg.getA());
+    printf("0x%04x\n", reg.getAF());
+    reg.setF(0x3A);
+    printf("0x%04x\n", reg.getAF());
+
+    reg.setAF(0x1234);
+    printf("0x%04x\n", reg.getAF());
+    printf("0x%02x\n", reg.getA());
+    printf("0x%02x\n", reg.getF());
+    
+    
+    
+    
+    
     return 0;
 }
